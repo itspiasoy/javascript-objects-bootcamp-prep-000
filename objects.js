6549ec45defabd20bf1ecd7e34fdf9a9bb69234c
Pia Soy <piasoy4@gmail.com>
2018-06-10 02:14:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-   playlist.artistName = songTitle
-
+  
+return Object.assign({},playlist, { [artistName]: songTitle })
 }
